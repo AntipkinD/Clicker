cf4ca1696ff38a1cf0bc0c1780246b48748622af
mushroomvoodoo <normeeboy@gmail.com>
2023-04-01 12:56:37 +0300

    end.

--- app/src/main/java/com/example/clicker/MainActivity.java
+++ app/src/main/java/com/example/clicker/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.clicker;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 import android.widget.TextView;
@@ -24,33 +23,13 @@ public class MainActivity extends Activity {
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState){
         super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("counter", counter);
     }
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-    }
-    @Override
-    public void onStart(){
-        super.onStart();
-    }
-    @Override
-    public void onResume(){
-        super.onResume();
-    }
-    @Override
-    public void onPause(){
-        super.onPause();
-    }
-    @Override
-    public void onStop(){
-        super.onStop();
-    }
-    @Override
-    public void onRestart() {
-        super.onRestart();
-    }
-    @Override
-    public void onDestroy(){
-        super.onDestroy();
+        counter = savedInstanceState.getInt("counter");
+        TextView counterView = findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
     }
 }
